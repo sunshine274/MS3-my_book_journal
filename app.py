8bee8329525ddd696edf2ac62d6d1d3938421c07
sunshine274 <jingx274@gmail.com>
2020-11-22 11:49:43 +0000

    added login functionality

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ def register():
         )
         if existing_user:
             flash("Username already exists!")
-            return redirect(url_for("register"))
-        
+            return redirect(url_for("login"))
+
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password")),
@@ -47,6 +47,32 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if user already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid Username and/or password
+                flash("incorrect Username and/or password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
